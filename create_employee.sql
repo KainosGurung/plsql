318210950a54786e9cb40ff3fd5a6ddeb497af07
Anon <kainosgurung@gmail.com>
2016-05-25 15:41:41 -0600

    removed dups

--- create_employee.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE employee(
-  ID NUMBER PRIMARY KEY,
-  fname VARCHAR2(30),
-  lname VARCHAR2(30),
-  age NUMBER CHECK (age >= 21),
-  sex VARCHAR2(10),
-  email VARCHAR2(100),
-  address VARCHAR2(50)  
-);
